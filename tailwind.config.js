5551416f40cfbd3b94e2373fa26f9de724d5ef7f
Nazar Nyzhnyk <nazarh2onyzhnyk@gmail.com>
2021-09-22 21:51:39 +0300

    mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          500: "#5345F5",
+        },
+        purple: {
+          500: "#7926FF",
+        },
+        gray: {
+          900: "#080514",
+        },
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
